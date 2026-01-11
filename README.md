ee277e3905cbc9b4cf89a05d5092c9445e392ee7
Lily Zhou <lzhouzyj@gmail.com>
2026-01-11 15:07:47 -0800

    Fixes in walkthrough; start on nanogpt impl + documentation

---
+++ README.md
@@ -0,0 +1,41 @@
+# NanoGPT
+
+This repo implements a simple GPT, based on Andrej Karpathy's [NanoGPT](https://github.com/karpathy/nanoGPT/tree/master).
+
+## GPT-walkthrough
+
+A good starting point is the [gpt-walkthrough.ipynb](gpt-walkthrough.ipynb) notebook, which contains notes from Andrej Karpathy's [Let's build GPT](https://www.youtube.com/watch?v=kCc8FmEb1nY&list=PLAqhIrjkxbuWI23v9cThsA9GvCAUhRvKZ&index=9) video. This notebook starts from a simple bigram model, then progressively introduces self-attention, multi-head self-attention, as well as optimizations such as LayerNorm. It also includes pretty detailed notes on each of the steps.
+
+The GPT in the notebook is a **character-level model** trained on **Jane Austen's *Emma***. By the end of the walkthrough, you can generate text like:
+
+```
+justicular
+imaginell the Frank
+Colong.
+Hence, you nother best, by the agreably extralled, and
+seceshamed Jane Fairfax at all only thoughth, it was scruptions, and _weather passionable that I am pain to you this declination?”—and worth him spiritéing servaking away, and she did I should never
+most desome once bethlunted it drequest anxion of
+farther, who concew any recoxcepte,
+in steased, one brought,” createst-Miss Bates’s were slight conshaded
+what is quite
+from any body’s one bring? who half in the great he kind; and
+solution, that to
+Mr. Elton issurple, his middles than understanding. He
+had
+only living us. She few the little, you short placle some
+days could her own used him; and all there
+is quite for so.
+Colongut sometingsfore her
+most to be disappoitations what is her
+father’s enjictely did spodering the nobody that she disgue, Jane, if he us, suffulnuicate Box to convise present it thought another. Mr. Cole, which she than Harreated to
+her worthing
+advarer.”
+
+“She safely, and c
+```
+
+## NanoGPT
+
+The [nanogpt](./nanogpt/) directory contains an implementation of NanoGPT.
+
+For a more detailed description of its implementation, see [nanogpt/README.md](./nanogpt/README.md).
---
+++ nanogpt/block.py
@@ -0,0 +1,163 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+class CausalSelfAttention(nn.Module):
+    """
+    A causal self-attention module.
+    """
+    def __init__(self, n_embd, n_head, block_size, dropout, bias):
+        super().__init__()
+
+        # This is necessary because head_size = n_embd // n_head
+        assert n_embd % n_head == 0
+
+        self.n_embd = n_embd
+        self.n_head = n_head
+        self.dropout = dropout
+
+        # Key, query, value projections for all heads in a batched linear layer;
+        # i.e. one big linear layer that computes Q, K, V for all heads at once.
+        # Input: (B, T, n_embd)
+        # Output: (B, T, 3*n_embd), which can be split into Q, K, V
+        # - Each Q, K, V contains the Q, K, V for all heads.
+        # - Shape of Q, K, V is (B, T, n_embd), which is equivalent to (B, T, n_head, head_size).
+        #   In forward(), we will transpose to get (B, n_head, T, head_size).
+        self.attention_layer = nn.Linear(n_embd, 3 * n_embd, bias=bias)
+
+        # Output projection: basically "mixing" the heads' outputs together.
+        # Input: (B, T, n_embd), i.e. (B, T, n_head * head_size)
+        # Output: (B, T, n_embd)
+        self.projection_layer = nn.Linear(n_embd, n_embd, bias)
+
+        # Regularization: dropout to prevent overfitting
+        self.attention_dropout = nn.Dropout(dropout)
+        self.resid_dropout = nn.Dropout(dropout)
+
+        # Causal mask: ensures that attention is only applied to the left ("previous")
+        # tokens in the input sequence.
+        tril = torch.tril(torch.ones(block_size, block_size)) # (T, T)
+        # Introduce batch and head dimensions to allow broadcasting
+        tril = tril.view(1, 1, block_size, block_size)        # (1, 1, T, T)
+        # Tell pytorch to not compute gradients for it
+        self.register_buffer("causal_mask", tril)
+    
+    def forward(self, input):
+        # B = batch size
+        # T = time = sequence length
+        # C = channel = embedding dimensionality (n_embd)
+        B, T, C = input.size()
+
+        # Get attention layer output and split into Q, K, V
+        # Input: (B, T, n_embd)
+        # Output: (B, T, 3 * n_embd)
+        # Split into Q, K, V: (B, T, n_embd), (B, T, n_embd), (B, T, n_embd)
+        q, k, v = self.attention_layer(input).split(self.n_embd, dim=2)
+
+        # Reshape Q, K, V into (B, T, n_head, head_size);
+        # then transpose into (B, n_head, T, head_size)
+        head_size = C // self.n_head
+        q = q.view(B, T, self.n_head, head_size).transpose(1, 2) # (B, n_head, T, head_size)
+        k = k.view(B, T, self.n_head, head_size).transpose(1, 2) # (B, n_head, T, head_size)
+        v = v.view(B, T, self.n_head, head_size).transpose(1, 2) # (B, n_head, T, head_size)
+
+        # Implement attention:
+
+        # (1) Compute attention scores ("affinities")
+        # (B, n_head, T, head_size) @ (B, n_head, head_size, T) => (B, n_head, T, T)
+        weights = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        weights = weights.masked_fill(self.causal_mask[:, :, :T, :T] == 0, float('-inf'))
+        weights = F.softmax(weights, dim=-1) # (B, n_head, T, T)
+        
+        # Dropout layer
+        weights = self.attention_dropout(weights)
+
+        # Output: attention_weights @ value
+        # (B, n_head, T, T) @ (B, n_head, T, head_size) => (B, n_head, T, head_size)
+        output = weights @ v
+
+        # Reassemble all head outputs side by side
+        # note: .contiguous() is required to call .view()
+        output = output.transpose(1, 2).contiguous() # (B, T, n_head, head_size)
+        output = output.view(B, T, C)                # (B, T, C) = (B, T, n_embd)
+
+        # Output projection: basically "mixing" the heads' outputs together
+        output = self.projection_layer(output)
+
+        # Residual dropout:
+        # this dropout is applied to the output before residual connection (in Block)
+        output = self.resid_dropout(output)
+        return output
+
+class LayerNorm(nn.Module):
+    """
+    In older version of PyTorch, nn.LayerNorm doesn't support bias=False.
+    So here, we write a customized LayerNorm with an optional bias.
+    """
+    def __init__(self, n_dim, bias = False):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(n_dim))
+        self.bias = nn.Parameter(torch.zeros(n_dim)) if bias else None
+
+    def forward(self, input):
+        return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
+
+class MLP(nn.Module):
+    """
+    This is the Feed-Forward Network (FFN) module.
+    It consists of 3 layers:
+    - Linear layer (expansion)
+    - GELU activation function
+    - Linear layer (projection)
+    """
+    def __init__(self, n_embd, dropout, bias = False):
+        super().__init__()
+
+        # Layer 1: linear layer (expansion)
+        # - expands dimensions from n_embd to 4 * n_embd
+        #   (B, T, n_embd) -> (B, T, 4 * n_embd)
+        # - "cfc" = "causal fully connected" (naming from GPT-2)
+        # - creates a wider hidden layer for more expressive computation
+        self.cfc = nn.Linear(n_embd, 4 * n_embd, bias=bias)
+
+        # Layer 2: non-linear activation (Gaussian Error Linear Unit)
+        # - smoother / performs better than ReLU
+        self.gelu = nn.GELU()
+
+        # Layer 3: linear layer (projection)
+        # - projects dimensions back to n_embd
+        #   (B, T, 4 * n_embd) -> (B, T, n_embd)
+        # - returns to original dimension to allow for residual connection
+        self.projection = nn.Linear(4 * n_embd, n_embd, bias=bias)
+
+        # Dropout layer: prevents overfitting
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        x = self.cfc(x)
+        x = self.gelu(x)
+        x = self.projection(x)
+        x = self.dropout(x)
+        return x
+
+class Block(nn.Module):
+    """
+    A transformer block that consists of:
+    - Layer Normalization
+    - Causal Self-Attention
+    - Layer Normalization
+    - Feed-Forward Network (MLP)
+    """
+    def __init__(self, n_embd, block_size, dropout, bias = False):
+        super().__init__()
+        self.ln_1 = LayerNorm(n_embd, bias=bias)
+        self.attn = CausalSelfAttention(n_embd, block_size, dropout, bias)
+        self.ln_2 = LayerNorm(n_embd, bias=bias)
+        self.mlp = MLP(n_embd, dropout, bias)
+
+    def forward(self, x):
+        # Apply residual connection
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
